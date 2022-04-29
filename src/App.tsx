f9c68f2cfff45af0a56da84bcfcc106cd1bd44fc
unknvn <91907825+unknvn@users.noreply.github.com>
2022-04-29 11:18:54 +0300

    animate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import CountUp from 'react-countup'
 import './App.css'
+
+const SECONDS_IN_YEAR = 31_536_000
 
 export default function App() {
   const birthdate = new Date('2002-11-24').getTime()
   const now = Date.now()
-  const age = (now - birthdate) / 31_536_000_000
+  const age = (now - birthdate) / SECONDS_IN_YEAR / 1000
 
-  return <div className="App">{age}</div>
+  return (
+    <div className="App">
+      <CountUp
+        start={age}
+        end={age + 1}
+        duration={SECONDS_IN_YEAR}
+        decimals={9}
+      />
+    </div>
+  )
 }
