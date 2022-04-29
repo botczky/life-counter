3b34da70c3def70dc6361a3dacb8015093757559
unknvn <91907825+unknvn@users.noreply.github.com>
2022-04-29 10:34:44 +0300

    age

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import './App.css'
+
 export default function App() {
-  return <div className="App">Hello Friends</div>
+  const birthdate = new Date('2002-11-24').getTime()
+  const now = Date.now()
+  const age = (now - birthdate) / 31_536_000_000
+
+  return <div className="App">{age}</div>
 }
