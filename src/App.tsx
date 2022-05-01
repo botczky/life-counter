6878ff66a66869a11dde62fff23256324114ce05
unknvn <91907825+unknvn@users.noreply.github.com>
2022-05-01 05:13:51 +0300

    color-scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { MantineProvider, Center } from '@mantine/core'
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+  Center,
+  Box,
+} from '@mantine/core'
 import { Calendar } from '@mantine/dates'
 import { useLocalStorage } from '@mantine/hooks'
 import { parseISO, formatISO } from 'date-fns'
 import Age from './components/Age'
+import ColorSchemeSwitch from './components/ColorSchemeSwitch'
 import './styles'
 
 export default function App() {
+  //
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'color-scheme',
+    defaultValue: 'dark',
+    getInitialValueInEffect: true,
+  })
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+
   const [birthdate, setBirthdate] = useLocalStorage<Date | null>({
     key: 'birthdate',
     defaultValue: null,
@@ -14,16 +31,31 @@ export default function App() {
   })
 
   return (
-    <MantineProvider
-      theme={{ fontFamilyMonospace: 'Azeret Mono' }}
-      withNormalizeCSS>
-      <Center sx={{ height: '100vh' }}>
-        {birthdate === null ? (
-          <Calendar value={birthdate} onChange={setBirthdate} />
-        ) : (
-          <Age birthdate={birthdate} />
-        )}
-      </Center>
-    </MantineProvider>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        theme={{ colorScheme, fontFamilyMonospace: 'Azeret Mono' }}
+        withNormalizeCSS
+        withGlobalStyles>
+        <Box sx={{ position: 'relative' }}>
+          <Center sx={{ height: '100vh' }}>
+            {birthdate === null ? (
+              <Calendar value={birthdate} onChange={setBirthdate} />
+            ) : (
+              <Age birthdate={birthdate} />
+            )}
+          </Center>
+          <Box
+            sx={(theme) => ({
+              position: 'absolute',
+              top: theme.spacing.xs,
+              right: theme.spacing.xs,
+            })}>
+            <ColorSchemeSwitch />
+          </Box>
+        </Box>
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
