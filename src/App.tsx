3a36656dfe42f543d235a8b15ca9475b50e2d722
unknvn <91907825+unknvn@users.noreply.github.com>
2022-05-07 14:11:33 +0300

    parcel

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import {
-  MantineProvider,
-  ColorSchemeProvider,
-  ColorScheme,
-} from '@mantine/core'
-import { useLocalStorage } from '@mantine/hooks'
+import { MantineProvider } from '@mantine/core'
 import MainContainer from './containers/MainContainer'
 import './styles'
 
 export default function App() {
-  //
-  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-    key: 'color-scheme',
-    defaultValue: 'dark',
-    getInitialValueInEffect: true,
-  })
-
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
-
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}>
-      <MantineProvider
-        theme={{ colorScheme, fontFamilyMonospace: 'Azeret Mono' }}
-        withNormalizeCSS
-        withGlobalStyles>
-        <MainContainer />
-      </MantineProvider>
-    </ColorSchemeProvider>
+    <MantineProvider
+      theme={{ fontFamilyMonospace: 'Azeret Mono' }}
+      withNormalizeCSS
+      withGlobalStyles>
+      <MainContainer />
+    </MantineProvider>
   )
 }
--- src/containers/MainContainer/MainContainer.tsx
+++ src/containers/MainContainer/MainContainer.tsx
@@ -1,21 +1,10 @@
-import { Center, Box } from '@mantine/core'
+import { Center } from '@mantine/core'
 import AgeContainer from '../AgeContainer'
-import ColorSchemeSwitch from './ColorSchemeSwitch'
 
 export default function MainContainer() {
   return (
-    <Box sx={{ position: 'relative' }}>
-      <Center sx={{ height: '100vh' }}>
-        <AgeContainer />
-      </Center>
-      <Box
-        sx={(theme) => ({
-          position: 'absolute',
-          top: theme.spacing.xs,
-          right: theme.spacing.xs,
-        })}>
-        <ColorSchemeSwitch />
-      </Box>
-    </Box>
+    <Center sx={{ height: '100vh' }}>
+      <AgeContainer />
+    </Center>
   )
 }
